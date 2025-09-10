8f044f84dc0062c0f74150da6c6cd76dd56fa7bb
Anirudh <anirudan2008@gmail.com>
2025-09-10 21:28:53 +0530

    implemented issue #3

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,17 +1,18 @@
 interface UserProps {
   username: string;
   avatarUrl: string;
+  githubUrl: string;
 }
 
 export default function User(props: UserProps) {
   return (
-    <div class="flex items-center space-x-4 rounded p-5 text-2xl transition hover:bg-slate-600">
+    <a href = {props.githubUrl} target="_blank" class="flex items-center space-x-4 rounded p-5 text-2xl transition hover:bg-slate-600">
       <img
         src={props.avatarUrl}
         alt="Discord Profile Picture"
         class="h-12 w-12 rounded-full shadow-md shadow-black"
       />
       <span class="text font-mono text-slate-200">{props.username}</span>
-    </div>
+    </a>
   );
 }
--- src/sections/Members.tsx
+++ src/sections/Members.tsx
@@ -9,30 +9,35 @@ export default function Members() {
           <User
             username="Thatmagicalcat"
             avatarUrl="https://cdn.discordapp.com/avatars/815189874478546954/fba107a0a4978c1e11063386102ba7ff.png?size=1024"
+            githubUrl="https://github.com/thatmagicalcat"
           />
         </li>
         <li>
           <User
             username="Wicks"
             avatarUrl="https://cdn.discordapp.com/avatars/1159829981803860009/80e4790205157fa6b4152f07c4f27ecf.png?size=1024"
+            githubUrl="https://github.com/Wickslynx"
           />
         </li>
         <li>
           <User
             username="DuskyElf"
             avatarUrl="https://cdn.discordapp.com/avatars/820957214264000513/fed1f113e379ef2b2921afb54b154394.png?size=1024"
+            githubUrl="https://github.com/DuskyElf"
           />
         </li>
         <li>
           <User
             username="ItsCoral"
             avatarUrl="https://cdn.discordapp.com/avatars/950347120962064464/460f6977493399d10b689174924db21f.png?size=1024"
+            githubUrl="https://github.com/ItsCoral0301"
           />
         </li>
         <li>
           <User
             username="The master of rick rolls"
             avatarUrl="https://cdn.discordapp.com/avatars/950521169432432700/92b68d623d59a2be9650b80999d0e345.png?size=1024"
+            githubUrl="https://github.com/Diego-evaristo-crtl"
           />
         </li>
       </ul>
