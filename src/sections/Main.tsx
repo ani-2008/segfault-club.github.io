20ea92365f7c2bed5a40cdf3926acc422d1fbffd
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-11-29 11:15:39 +0530

    Fade in animations hehe

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -8,7 +8,7 @@ export default function Main(props: MainProps) {
       class={`flex snap-start items-center px-[8%] text-white ${props.className}`}
     >
       <main class="space-y-7">
-        <h1 class="font-logo text-7xl md:text-8xl text-violet-50">
+        <h1 class="font-logo fade-down text-7xl text-violet-50 md:text-8xl">
           Seg
           <span class="text-red-400 underline decoration-red-400 decoration-wavy decoration-2">
             fault
@@ -16,14 +16,16 @@ export default function Main(props: MainProps) {
           Club
         </h1>
         <div class="space-y-4 font-mono text-lg md:text-xl">
-          <p>Linux, Debugging, Chaos and legendary segfaults</p>
+          <p class="fade-down [--fade-in-delay:_50ms]">
+            Linux, Debugging, Chaos and legendary segfaults
+          </p>
           <a
-            class="rounded bg-slate-700 p-2 transition hover:bg-slate-600"
+            class="fade-down rounded bg-slate-700 p-2 transition [--fade-in-delay:_300ms] hover:bg-slate-600"
             href="https://discord.gg/gohar"
             target="_blank"
           >
             Join{" "}
-            <span class="underline decoration-blue-400 decoration-2">
+            <span class="fade-down underline decoration-blue-400 decoration-2 [--fade-in-delay:_400ms]">
               @discord.gg/gohar
             </span>
           </a>
