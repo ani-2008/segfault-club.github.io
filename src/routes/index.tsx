5b3778f85d0d13fe657c67dec93936b34a33ef4e
bitabyte <bitabyte7@gmail.com>
2026-04-22 17:33:32 +0530

    Remove committe members section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -40,64 +40,6 @@ export default function index() {
           </div>
         </main>
       </section>
-
-      <section class="h-screen w-full snap-start overflow-y-scroll bg-slate-900/60 p-4 text-white backdrop-blur-sm [scrollbar-width:none]">
-        <h2 class="m-6 text-center text-4xl font-bold text-slate-300 underline underline-offset-10">
-          Our Committee Members
-        </h2>
-        <ul class="mg:gap-7 mx-auto mt-15 flex max-w-3/4 flex-wrap items-center justify-center lg:gap-12">
-          <li>
-            <User
-              username="Thatmagicalcat"
-              avatarUrl="https://avatars.githubusercontent.com/u/85670037"
-              githubUrl="https://github.com/thatmagicalcat"
-            />
-          </li>
-          <li>
-            <User
-              username="Wicks"
-              avatarUrl="https://avatars.githubusercontent.com/u/167521638"
-              githubUrl="https://codeberg.org/Wicks"
-            />
-          </li>
-          <li>
-            <User
-              username="DuskyElf"
-              avatarUrl="https://avatars.githubusercontent.com/u/91879372"
-              githubUrl="https://github.com/DuskyElf"
-            />
-          </li>
-          <li>
-            <User
-              username="Diego Evaristo"
-              avatarUrl="https://avatars.githubusercontent.com/u/186722487"
-              githubUrl="https://github.com/foo8664"
-            />
-          </li>
-          <li>
-            <User
-              username="infiKNIGHTe"
-              avatarUrl="https://avatars.githubusercontent.com/u/142007233"
-              githubUrl="https://github.com/infiknighte"
-            />
-          </li>
-          <li>
-            <User
-              username="AshyPingu"
-              avatarUrl="https://codeberg.org/avatars/34304f24b374b632b8f47d7eac10524a55bc00bf8e9060b3466cfda26fa54310?size=512"
-              githubUrl="https://codeberg.org/AshyPinguin"
-              // githubUrl="https://github.com/PinguinLars"
-            />
-          </li>
-          <li>
-            <User
-              username="bitabyte"
-              avatarUrl="https://avatars.githubusercontent.com/u/95073991"
-              githubUrl="https://github.com/ani-2008"
-            />
-          </li>
-        </ul>
-      </section>
     </>
   );
 }
